6112bf438322aed11dd344fee901a8068b08c6ff
Yogesh Sonawane <yogesh.sonawane@invasystems.com>
2023-08-25 20:42:11 +0530

    Add project files.

---
+++ AdventureDB2023/AdventureDB2023/dbo/Stored Procedures/Procedure1.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[Procedure1]
+	@param1 int = 0,
+	@param2 int
+AS
+	SELECT @param1, @param2
+
+	 Select getdate()--added for pull
+	 
+
+
+
+
+
+RETURN 0
---
+++ AdventureDB2023/AdventureDB2023/dbo/Stored Procedures/Procedure2.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[Procedure2]
+	@param1 int = 0,
+	@param2 int
+AS
+
+	 Select getdate()
+	SELECT @param1, @param2
+RETURN 0
